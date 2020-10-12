f6e22ffadc13b0a6f98456345adfcdd39f2e2bca
Patrick Lombardo <patrickjohnlombardo@gmail.com>
2020-10-12 10:27:27 -0700

    add submit post functionality

--- src/UI.js
+++ src/UI.js
@@ -30,6 +30,45 @@ class UI {
 
     this.post.innerHTML = output;
   }
+
+  showAlert(message, className) {
+    this.clearAlert();
+
+    // Create div
+    const div = document.createElement("div");
+
+    // Add Classes
+    div.className = className;
+
+    // Add text
+    div.appendChild(document.createTextNode(message));
+
+    // Get Parent
+    const container = document.querySelector(".postsContainer");
+
+    // Get Posts
+    const posts = document.querySelector("#posts");
+
+    // Insert Alert div before posts
+    container.insertBefore(div, posts);
+
+    // Timeout - remove after 3 seconds
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+
+  clearAlert() {
+    const currentAlert = document.querySelector(".alert");
+    if (currentAlert) {
+      currentAlert.remove();
+    }
+  }
+
+  clearFields() {
+    this.titleInput.value = "";
+    this.bodyInput.value = "";
+  }
 }
 
 export const ui = new UI();
